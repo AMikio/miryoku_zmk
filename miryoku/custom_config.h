d3182cd10a2dd8d18278074fb3b11ad5d3f5870b
AMikio <42617257+AMikio@users.noreply.github.com>
2024-06-10 13:56:31 -0300

    Use Miryoku defined key codes for clipboard on default layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -17,8 +17,8 @@
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
 &kp ESC K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp C_VOL_UP \
-&kp K_PASTE  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp C_VOL_DN \
-&kp K_COPY  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp C_MUTE \
+U_PST  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp C_VOL_DN \
+U_CPY  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp C_MUTE \
                K32  K33  K34       K35  K36  K37
 
 #endif
