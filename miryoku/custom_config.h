87bd10fe29fa33a1b1a160cca70000697e9a63d0
AMikio <shimonishimikio@gmail.com>
2026-02-04 10:22:18 -0300

    fix: fix misnamed variables

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -101,7 +101,7 @@
 
 #define XXX &none
 
-#define MIRYOKU_LAYOUTMAPPING_BASE(                                            \
+#define MIRYOKU_LAYERMAPPING_BASE(                                            \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12, K13, K14, \
     K15, K16, K17, K18, K19, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, \
                   N30, N31, K32, K33, K34, K35, K36, K37, N38, N39)          \
@@ -111,7 +111,7 @@
    &kp U_UND K20         K21         K22         K23         K24        &kp U_UND       &kp U_RDO   K25         K26         K27         K28         K29        &kp U_RDO      \
                                      XXX         K32         K33        K34                   K35   K36         K37         XXX
 
-#define MIRYOKU_LAYOUTMAPPING_NUM(                                             \
+#define MIRYOKU_LAYERMAPPING_NUM(                                             \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12, K13, K14, \
     K15, K16, K17, K18, K19, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, \
     N30, N31, K32, K33, K34, K35, K36, K37, N38, N39)                          \
@@ -121,7 +121,7 @@
    &kp U_UND   K20         K21         K22         K23         K14        &kp U_UND     &kp U_RDO   K25         K26         K27         K28         K29        &kp U_RDO      \
                                        XXX         K33         K32        K34                 K35   K36         K37         XXX
 
-#define MIRYOKU_LAYOUTMAPPING_SYM(                                             \
+#define MIRYOKU_LAYERMAPPING_SYM(                                             \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12, K13, K14, \
     K15, K16, K17, K18, K19, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, \
     N30, N31, K32, K33, K34, K35, K36, K37, N38, N39)                          \
@@ -131,7 +131,7 @@
    &kp U_UND   K20         K21         K22         K23         K14       &kp U_UND      &kp U_RDO   K25         K26         K27         K28         K29        &kp U_RDO      \
                                        XXX         K32         K33       K34                  K35   K36         K37         XXX
 
-#define MIRYOKU_LAYOUTMAPPING_FUN(                                             \
+#define MIRYOKU_LAYERMAPPING_FUN(                                             \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12, K13, K14, \
     K15, K16, K17, K18, K19, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, \
     N30, N31, K32, K33, K34, K35, K36, K37, N38, N39)                          \
