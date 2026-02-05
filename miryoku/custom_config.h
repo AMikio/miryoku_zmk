91c4a045d8f417fde523e2464b2959ae957eafea
AMikio <shimonishimikio@gmail.com>
2026-02-05 00:58:04 -0300

    feat: add game layer to lily58

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,8 +6,6 @@
 #define MIRYOKU_EXTRA_QWERTY
 #define MIRYOKU_NAV_VI
 
-#if defined(MIRYOKU_KEYBOARD_CORNE)
-
 #define XXX &none
 
 // FPS friendly tap layer
@@ -18,13 +16,45 @@
       &kp DOT, &kp SLASH, U_NP, U_NP, &kp LALT, &kp SPC, &mo U_GAMENUM,        \
       &kp RET, &kp BSPC, &kp DEL, U_NP, U_NP
 
-
 #define MIRYOKU_LAYER_GAMENUM                                                  \
   &kp ESC, &kp N1, &kp N2, &kp N3, &kp T, &kp LBKT, &kp F7, &kp F8, &kp F9,    \
       &kp RBKT, &kp LSHFT, &kp N4, &kp N5, &kp N6, &kp G, &kp EQL, &kp F4,     \
       &kp F5, &kp F6, &kp SEMI, &kp LCTRL, &kp N7, &kp N8, &kp N9, &kp B,      \
       &kp BSLH, &kp F1, &kp F2, &kp F3, &kp GRAVE, U_NP, U_NP, &to U_BASE,     \
       &kp SPC, &none, &kp MINUS, &kp N0, &kp DOT, U_NP, U_NP
+
+#define MIRYOKU_LAYERMAPPING_GAMENUM MIRYOKU_MAPPING
+
+#define MIRYOKU_LAYER_LIST                                                     \
+  MIRYOKU_X(BASE, "Base")                                                      \
+  MIRYOKU_X(EXTRA, "Extra")                                                    \
+  MIRYOKU_X(TAP, "Tap")                                                        \
+  MIRYOKU_X(BUTTON, "Button")                                                  \
+  MIRYOKU_X(NAV, "Nav")                                                        \
+  MIRYOKU_X(MOUSE, "Mouse")                                                    \
+  MIRYOKU_X(MEDIA, "Media")                                                    \
+  MIRYOKU_X(NUM, "Num")                                                        \
+  MIRYOKU_X(SYM, "Sym")                                                        \
+  MIRYOKU_X(FUN, "Fun")                                                        \
+  MIRYOKU_X(GAME, "Game")                                                      \
+  MIRYOKU_X(GAMENUM, "GNum")
+
+#define U_BASE 0
+#define U_EXTRA 1
+#define U_TAP 2
+#define U_BUTTON 3
+#define U_NAV 4
+#define U_MOUSE 5
+#define U_MEDIA 6
+#define U_NUM 7
+#define U_SYM 8
+#define U_FUN 9
+#define U_GAME 10
+#define U_GAMENUM 11
+
+#if defined(MIRYOKU_KEYBOARD_CORNE)
+
+// clang-format off
 
 #define MIRYOKU_LAYERMAPPING_BASE(                                                \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12, K13, K14,    \
@@ -70,80 +100,73 @@
   U_PST        K10 K11 K12 K13 K14          K15 K16 K17 K18 K19 &kp C_VOL_DN      \
   U_CPY        K20 K21 K22 K23 K24          K25 K26 K27 K28 K29 &kp C_MUTE        \
                        K32 K33 K34          K35 K36 K37
-
-#define MIRYOKU_LAYERMAPPING_GAMENUM MIRYOKU_MAPPING
-
-#define MIRYOKU_LAYER_LIST   \
-MIRYOKU_X(BASE,   "Base")    \
-MIRYOKU_X(EXTRA,  "Extra")   \
-MIRYOKU_X(TAP,    "Tap")     \
-MIRYOKU_X(BUTTON, "Button")  \
-MIRYOKU_X(NAV,    "Nav")     \
-MIRYOKU_X(MOUSE,  "Mouse")   \
-MIRYOKU_X(MEDIA,  "Media")   \
-MIRYOKU_X(NUM,    "Num")     \
-MIRYOKU_X(SYM,    "Sym")     \
-MIRYOKU_X(FUN,    "Fun")     \
-MIRYOKU_X(GAME,   "Game")    \
-MIRYOKU_X(GAMENUM,"GNum")
-
-#define U_BASE        0
-#define U_EXTRA       1
-#define U_TAP         2
-#define U_BUTTON      3
-#define U_NAV         4
-#define U_MOUSE       5
-#define U_MEDIA       6
-#define U_NUM         7
-#define U_SYM         8
-#define U_FUN         9
-#define U_GAME       10
-#define U_GAMENUM    11
-
+// clang format on
 #endif
 
 #if defined(MIRYOKU_KEYBOARD_LILY58)
 
 #define XXX &none
 
+// clang-format off
 #define MIRYOKU_LAYERMAPPING_BASE(                                             \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12, K13, K14, \
     K15, K16, K17, K18, K19, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, \
                 N30, N31, K32, K33, K34, K35, K36, K37, N38, N39)              \
-   &kp ESC   &kp N1      &kp N2      &kp N3      &kp N4      &kp N5                                 &kp N6      &kp N7      &kp N8      &kp N9      &kp N0     &kp C_MUTE     \
-   U_PST     K00         K01         K02         K03         K04                                    K05         K06         K07         K08         K09        &kp C_VOL_UP   \
-   U_CPY     K10         K11         K12         K13         K14                                    K15         K16         K17         K18         K19        &kp C_VOL_DN   \
-   U_UND     K20         K21         K22         K23         K24        U_UND               U_RDO   K25         K26         K27         K28         K29        U_RDO          \
-                                     XXX         K32         K33        K34                   K35   K36         K37         XXX
+   &u_to_U_GAME   &kp N1      &kp N2      &kp N3      &kp N4      &kp N5                                 &kp N6      &kp N7      &kp N8      &kp N9      &kp N0     &kp C_MUTE     \
+   U_PST          K00         K01         K02         K03         K04                                    K05         K06         K07         K08         K09        &kp C_VOL_UP   \
+   U_CPY          K10         K11         K12         K13         K14                                    K15         K16         K17         K18         K19        &kp C_VOL_DN   \
+   U_UND          K20         K21         K22         K23         K24        U_UND               U_RDO   K25         K26         K27         K28         K29        U_RDO          \
+                                          XXX         K32         K33        K34                   K35   K36         K37         XXX
 
 #define MIRYOKU_LAYERMAPPING_NUM(                                              \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12, K13, K14, \
     K15, K16, K17, K18, K19, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, \
                 N30, N31, K32, K33, K34, K35, K36, K37, N38, N39)              \
-   &kp ESC     &kp N1      &kp N2      &kp N3      &kp N4      &kp N5                               &kp N6      &kp N7      &kp N8      &kp N9      &kp N0     &kp C_MUTE     \
-   U_PST       K00         K01         K02         K03         K24                                  K05         K06         K07         K08         K09        &kp C_VOL_UP   \
-   U_CPY       K10         K11         K12         K13         K00                                  K04         K16         K17         K18         K19        &kp C_VOL_DN   \
-   U_UND       K20         K21         K22         K23         K14        U_UND             U_RDO   K25         K26         K27         K28         K29        U_RDO          \
-                                       XXX         K33         K32        K34                 K35   K36         K37         XXX
+   &kp ESC        &kp N1      &kp N2      &kp N3      &kp N4      &kp N5                               &kp N6      &kp N7      &kp N8      &kp N9      &kp N0     &kp C_MUTE     \
+   U_PST          K00         K01         K02         K03         K24                                  K05         K06         K07         K08         K09        &kp C_VOL_UP   \
+   U_CPY          K10         K11         K12         K13         K00                                  K04         K16         K17         K18         K19        &kp C_VOL_DN   \
+   U_UND          K20         K21         K22         K23         K14        U_UND             U_RDO   K25         K26         K27         K28         K29        U_RDO          \
+                                          XXX         K33         K32        K34                 K35   K36         K37         XXX
 
 #define MIRYOKU_LAYERMAPPING_SYM(                                              \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12, K13, K14, \
     K15, K16, K17, K18, K19, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, \
                 N30, N31, K32, K33, K34, K35, K36, K37, N38, N39)              \
-   &kp ESC     &kp N1      &kp N2      &kp N3      &kp N4      &kp N5                               &kp N6      &kp N7      &kp N8      &kp N9      &kp N0     &kp C_MUTE     \
-   U_PST       K00         K01         K02         K03         K24                                  K05         K06         K07         K08         K09        &kp C_VOL_UP   \
-   U_CPY       K10         K11         K12         K13         K32                                  K33         K16         K17         K18         K19        &kp C_VOL_DN   \
-   U_UND       K20         K21         K22         K23         K14       U_UND              U_RDO   K25         K26         K27         K28         K29        U_RDO          \
-                                       XXX         K32         K33       K34                  K35   K36         K37         XXX
+   &kp ESC        &kp N1      &kp N2      &kp N3      &kp N4      &kp N5                               &kp N6      &kp N7      &kp N8      &kp N9      &kp N0     &kp C_MUTE     \
+   U_PST          K00         K01         K02         K03         K24                                  K05         K06         K07         K08         K09        &kp C_VOL_UP   \
+   U_CPY          K10         K11         K12         K13         K32                                  K33         K16         K17         K18         K19        &kp C_VOL_DN   \
+   U_UND          K20         K21         K22         K23         K14       U_UND              U_RDO   K25         K26         K27         K28         K29        U_RDO          \
+                                          XXX         K32         K33       K34                  K35   K36         K37         XXX
 
 #define MIRYOKU_LAYERMAPPING_FUN(                                              \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12, K13, K14, \
     K15, K16, K17, K18, K19, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, \
                 N30, N31, K32, K33, K34, K35, K36, K37, N38, N39)              \
-   &kp ESC     &kp N1      &kp N2      &kp N3      &kp N4      &kp N5                               &kp N6      &kp N7      &kp N8      &kp N9      &kp N0     &kp C_MUTE      \
-   U_PST       K00         K01         K02         K03         K24                                  K05         K06         K07         K08         K09        &kp C_VOL_UP    \
-   U_CPY       K10         K11         K12         K13         &kp LBRC                             &kp RBRC    K16         K17         K18         K19        &kp C_VOL_DN    \
-   U_UND       K20         K21         K22         K23         K24      U_UND               U_RDO   K25         K26         K27         K28         K29        U_RDO           \
-                                       XXX         K32         K33      K34                   K35   K36         K37         XXX
+   &kp ESC        &kp N1      &kp N2      &kp N3      &kp N4      &kp N5                               &kp N6      &kp N7      &kp N8      &kp N9      &kp N0     &kp C_MUTE      \
+   U_PST          K00         K01         K02         K03         K24                                  K05         K06         K07         K08         K09        &kp C_VOL_UP    \
+   U_CPY          K10         K11         K12         K13         &kp LBRC                             &kp RBRC    K16         K17         K18         K19        &kp C_VOL_DN    \
+   U_UND          K20         K21         K22         K23         K24      U_UND               U_RDO   K25         K26         K27         K28         K29        U_RDO           \
+                                          XXX         K32         K33      K34                   K35   K36         K37         XXX
+
+#define MIRYOKU_LAYERMAPPING_GAME(                                             \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12, K13, K14, \
+    K15, K16, K17, K18, K19, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, \
+                N30, N31, K32, K33, K34, K35, K36, K37, N38, N39)              \
+   &u_to_U_BASE   &kp N1      &kp N2      &kp N3      &kp N4      &kp N5                                 &kp N6      &kp N7      &kp N8      &kp N9      &kp N0     &kp C_MUTE     \
+   U_PST          K00         K01         K02         K03         K04                                    K05         K06         K07         K08         K09        &kp C_VOL_UP   \
+   U_CPY          K10         K11         K12         K13         K14                                    K15         K16         K17         K18         K19        &kp C_VOL_DN   \
+   U_UND          K20         K21         K22         K23         K24        U_UND               U_RDO   K25         K26         K27         K28         K29        U_RDO          \
+                                          XXX         K32         K33        K34                   K35   K36         K37         XXX
+
+#define MIRYOKU_LAYERMAPPING_GAMENUM(                                             \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12, K13, K14, \
+    K15, K16, K17, K18, K19, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, \
+                N30, N31, K32, K33, K34, K35, K36, K37, N38, N39)              \
+   &u_to_U_BASE   &kp F1      &kp F2      &kp F3      &kp F4      &kp F5                                 &kp F6      &kp F7      &kp F8      &kp F9      &kp F10    &kp C_MUTE     \
+   U_PST          K00         K01         K02         K03         K04                                    K05         K06         K07         K08         K09        &kp C_VOL_UP   \
+   U_CPY          K10         K11         K12         K13         K14                                    K15         K16         K17         K18         K19        &kp C_VOL_DN   \
+   U_UND          K20         K21         K22         K23         K24        U_UND               U_RDO   K25         K26         K27         K28         K29        U_RDO          \
+                                          XXX         K32         K33        K34                   K35   K36         K37         XXX
+// clang-format on
 
 #endif
