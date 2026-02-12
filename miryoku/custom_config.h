3ad148ab110fa4d45be4dba62a0e55e6d2964773
AMikio <shimonishimikio@gmail.com>
2026-02-12 15:08:10 -0300

    fix: fix wrong keycode in FUN layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -144,7 +144,7 @@
     K15, K16, K17, K18, K19, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, \
                 N30, N31, K32, K33, K34, K35, K36, K37, N38, N39)              \
    &kp ESC        &kp F1      &kp F2      &kp F3      &kp F4      &kp F5                                 &kp F6      &kp F7      &kp F8      &kp F9      &kp F10    &kp C_MUTE      \
-   &kp PG_UP      K00         K01         K02         K03         K24                                    K05         K06         K07         K08         K09        &kp C_VOL_UP    \
+   &kp PG_UP      K00         K01         K02         K03         K04                                    K05         K06         K07         K08         K09        &kp C_VOL_UP    \
    &kp PG_DN      K10         K11         K12         K13         &kp LBRC                               &kp RBRC    K16         K17         K18         K19        &kp C_VOL_DN    \
    U_UND          K20         K21         K22         K23         K24        U_CPY             U_PST     K25         K26         K27         K28         K29        U_RDO           \
                                           &kp HOME    K32         K33        K34                 K35     K36         K37         &kp END
